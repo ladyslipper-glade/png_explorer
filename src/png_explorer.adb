7aa63cfd29b966e90e33f02eb9d89f49f4a7d739
Anne Chapin <217229590+ladyslipper-glade@users.noreply.github.com>
2025-12-02 21:02:58 -0500

    Main program reads a file one byte at a time.

--- src/png_explorer.adb
+++ src/png_explorer.adb
@@ -1,4 +1,20 @@
+with Ada.Sequential_IO;
+
 procedure Png_Explorer is
+   type Byte_Type is mod 2**8;
+   package Byte_IO is new Ada.Sequential_IO (Byte_Type);
+
+   File : Byte_IO.File_Type;
+   Byte : Byte_Type;
 begin
-   null;
+   Byte_IO.Open (File, Byte_IO.In_File, "tardigradum.png");
+
+   while not Byte_IO.End_Of_File (File) loop
+      Byte_IO.Read (File, Byte);
+
+      --  Process byte
+
+   end loop;
+
+   Byte_IO.Close (File);
 end Png_Explorer;
